fff5d9eecded9f2bb8b0a2eb8a2619b561c5ee03
Brenda Taveras <brendamt1@gmail.com>
2016-09-21 13:55:21 -0400

    Add comments to partDriver class

--- src/Orchard.Web/Modules/Pluralsight.Movies/Drivers/MoviePartDriver.cs
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Drivers/MoviePartDriver.cs
@@ -16,6 +16,8 @@ namespace Pluralsight.Movies.Drivers
         }
 
         //GET
+        //orchard calls this when it loads the form or when save is there is
+        //not a corresponding Editor method for such action
         protected override DriverResult Editor(MoviePart part, dynamic shapeHelper)
         {
             return ContentShape("Parts_Movie_Edit", () =>
@@ -24,10 +26,12 @@ namespace Pluralsight.Movies.Drivers
         }
 
         //POST 
+        //was added so that orchard calls this instead when saving
+        //map values posted to part with updater
         protected override DriverResult Editor(MoviePart part, IUpdateModel updater, dynamic shapeHelper)
         {
             updater.TryUpdateModel(part, Prefix, null, null);
-            return Editor(part, shapeHelper);
+            return Editor(part, shapeHelper); //call get method to redisplay form
         }
     }
 }
