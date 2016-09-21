6fa0b5527abfbf4e72d8f941ee0d49e7313d0d25
Brenda Taveras <brendamt1@gmail.com>
2016-09-21 13:30:08 -0400

    Add Module.txt and Migration.cs

---
+++ src/Orchard.Web/Modules/Pluralsight.Movies/Migrations.cs
@@ -0,0 +1,20 @@
+﻿using Orchard.Data.Migration;
+using Orchard.ContentManagement.MetaData;
+using Orchard.Core.Contents.Extensions;
+
+namespace Pluralsight.Movies
+{
+    public class Migrations : DataMigrationImpl
+    {
+        public int Create()
+        {
+            ContentDefinitionManager.AlterTypeDefinition(
+                "Movie", builder => builder.WithPart(
+                    "CommonPart").WithPart("TitlePart").
+                    WithPart("AutoroutePart")
+                );
+            return 1;
+        }
+
+    }
+}
